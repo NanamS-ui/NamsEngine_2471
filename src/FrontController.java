abdc04ac0d6546ade9f405eab5aef2a0ad5a8f06
NanamS-ui <rahajamananaralisontoky@gmail.com>
2024-11-15 21:22:09 +0300

    sprint13_2471

--- src/FrontController.java
+++ src/FrontController.java
@@ -1,21 +1,22 @@
 package controller;
 
 import annotation.*;
+import annotation.ValidationAnnotations.*;
 import utils.*;
+import exception.ValidationException;
+import com.google.gson.Gson;
+import exception.*;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.*;
 import java.util.HashMap;
-import exception.*;
-import javax.servlet.http.Part;
-import com.google.gson.Gson;
 
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, maxFileSize = 1024 * 1024 * 50, maxRequestSize = 1024 * 1024
+        * 100)
 public class FrontController extends HttpServlet {
     private HashMap<String, Mapping> hashMap;
 
@@ -30,117 +31,102 @@ public class FrontController extends HttpServlet {
         }
     }
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
+        try (PrintWriter out = response.getWriter()) {
+            String url = request.getRequestURI().substring(request.getContextPath().length());
+            System.out.println("Request URI: " + url);
 
-        String url = request.getRequestURI().substring(request.getContextPath().length());
-        System.out.println("Request URI: " + url);
-
-        if (hashMap != null) {
-            Mapping mapping = hashMap.get(url);
-            if (mapping != null) {
-                try {
-                    Class<?> clazz = Class.forName(mapping.getClassName());
-                    Method method = null;
-
-                    // Cherche la méthode dans la classe correspondante
-                    for (Method m : clazz.getDeclaredMethods()) {
-                        if (m.getName().equals(mapping.getMethodName())) {
-                            method = m;
-                            break;
-                        }
-                    }
-
-                    if (method == null) {
-                        throw new NoSuchMethodException(
-                                "Method " + mapping.getMethodName() + " not found in " + clazz.getName());
-                    }
-
-                    // Vérifie si la requête correspond au verbe de la méthode
-                    String requestMethod = request.getMethod();
-                    String methodVerb = mapping.getVerb();
-
-                    if (!methodVerb.equalsIgnoreCase(requestMethod) && !methodVerb.isEmpty()) {
-                        response.sendError(HttpServletResponse.SC_BAD_REQUEST,
-                                "Method not allowed: " + requestMethod + " for URL: " + url);
-                        return;
-                    }
-
-                    Object instance = clazz.getDeclaredConstructor().newInstance();
-                    Object[] parameterValues = Utils.getParameterValues(request, method, Param.class,
-                            ParamObject.class);
-
-                    if (request.getContentType() != null && request.getContentType().startsWith("multipart/")) {
-                        for (int i = 0; i < parameterValues.length; i++) {
-                            if (parameterValues[i] == null && method.getParameterTypes()[i].equals(MyMultiPart.class)) {
-                                Part filePart = request.getPart(method.getParameters()[i].getName());
-                                if (filePart != null) {
-                                    parameterValues[i] = new MyMultiPart(filePart);
-                                }
-                            }
-                        }
-                    }
-
-                    // Initialisation de MySession dans un attribut
-                    Field sessionField = null;
-                    for (Field field : clazz.getDeclaredFields()) {
-                        if (field.getType().equals(MySession.class)) {
-                            sessionField = field;
-                            break;
-                        }
-                    }
-                    if (sessionField != null) {
-                        sessionField.setAccessible(true);
-                        sessionField.set(instance, new MySession(request.getSession()));
-                    }
-
-                    // Initialisation de MySession dans un paramètre
-                    for (int i = 0; i < parameterValues.length; i++) {
-                        if (parameterValues[i] == null && method.getParameterTypes()[i].equals(MySession.class)) {
-                            MySession session = new MySession(request.getSession());
-                            parameterValues[i] = session;
-                        }
-                    }
-
-                    Object result = method.invoke(instance, parameterValues);
-                    if (method.isAnnotationPresent(ResponseBody.class)) {
-                        if (result instanceof ModelView) {
-                            ModelView modelView = (ModelView) result;
-                            RequestDispatcher dispatch = request.getRequestDispatcher(modelView.getUrl());
-                            HashMap<String, Object> data = modelView.getData();
-                            Gson gson = new Gson();
-                            String jsonResponse = gson.toJson(data);
-                            out.println(jsonResponse);
-                        } else {
-                            Gson gson = new Gson();
-                            String jsonResponse = gson.toJson(result);
-                            out.println(jsonResponse);
-                        }
-                    } else if (result instanceof ModelView) {
-                        ModelView modelView = (ModelView) result;
-                        RequestDispatcher dispatch = request.getRequestDispatcher(modelView.getUrl());
-                        HashMap<String, Object> data = modelView.getData();
-                        for (String keyData : data.keySet()) {
-                            request.setAttribute(keyData, data.get(keyData));
-                            System.out.println(keyData);
-                        }
-                        dispatch.forward(request, response);
-                    } else if (result instanceof String) {
-                        out.println(result.toString());
-                    } else {
-                        out.println("Unsupported return type from controller method.");
-                    }
-                } catch (Exception e) {
-                    out.println("Error invoking method: " + e.getMessage());
-                }
-            } else {
-                response.sendError(HttpServletResponse.SC_NOT_FOUND, "No mapping found for URL: " + url);
+            if (hashMap == null) {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                        "HashMap is null. Initialization failed.");
+                return;
             }
+
+            Mapping mapping = hashMap.get(url);
+            if (mapping == null) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "No mapping found for URL: " + url);
+                return;
+            }
+
+            try {
+                Class<?> clazz = Class.forName(mapping.getClassName());
+                Method method = findMethod(clazz, mapping.getMethodName());
+                if (method == null) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                            "Method not found in class: " + clazz.getName());
+                    return;
+                }
+
+                validateRequestVerb(request, mapping.getVerb(), url);
+
+                Object instance = clazz.getDeclaredConstructor().newInstance();
+                injectSession(instance, request);
+                Object[] parameterValues = Utils.getParameterValues(request, method, Param.class, ParamObject.class);
+
+                Object result = method.invoke(instance, parameterValues);
+
+                handleResponse(result, request, response, out, method);
+
+            } catch (Exception e) {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                        "Error processing request: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Method findMethod(Class<?> clazz, String methodName) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getName().equals(methodName)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    private void validateRequestVerb(HttpServletRequest request, String expectedVerb, String url)
+            throws ServletException {
+        String requestMethod = request.getMethod();
+        if (!expectedVerb.equalsIgnoreCase(requestMethod) && !expectedVerb.isEmpty()) {
+            throw new ServletException("Method not allowed: " + requestMethod + " for URL: " + url);
+        }
+    }
+
+    private void injectSession(Object instance, HttpServletRequest request) throws IllegalAccessException {
+        Field sessionField = null;
+        for (Field field : instance.getClass().getDeclaredFields()) {
+            if (field.getType().equals(MySession.class)) {
+                sessionField = field;
+                break;
+            }
+        }
+        if (sessionField != null) {
+            sessionField.setAccessible(true);
+            sessionField.set(instance, new MySession(request.getSession()));
+        }
+    }
+
+    private void handleResponse(Object result, HttpServletRequest request, HttpServletResponse response,
+            PrintWriter out,
+            Method method) throws IOException, ServletException {
+        if (method.isAnnotationPresent(ResponseBody.class)) {
+            Gson gson = new Gson();
+            String jsonResponse = gson.toJson(result);
+            out.println(jsonResponse);
+        } else if (result instanceof ModelView) {
+            ModelView modelView = (ModelView) result;
+            RequestDispatcher dispatcher = request.getRequestDispatcher(modelView.getUrl());
+            HashMap<String, Object> data = modelView.getData();
+            for (String key : data.keySet()) {
+                request.setAttribute(key, data.get(key));
+            }
+            dispatcher.forward(request, response);
+        } else if (result instanceof String) {
+            out.println(result.toString());
         } else {
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                    "HashMap is null. Initialization may have failed.");
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unsupported return type.");
         }
     }
 
@@ -155,4 +141,5 @@ public class FrontController extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
+
 }
--- src/Utils.java
+++ src/Utils.java
@@ -1,79 +1,181 @@
 package utils;
 
 import javax.servlet.http.HttpServletRequest;
-import com.thoughtworks.paranamer.*;
-import java.lang.reflect.*;
-
+import javax.servlet.ServletException;
+import javax.servlet.http.Part;
 import annotation.*;
+import annotation.ValidationAnnotations.*;
+import exception.*;
+import java.lang.reflect.*;
+import java.util.regex.Matcher;
+import java.io.*;
 
 public class Utils {
+
+    /**
+     * Récupère les valeurs des paramètres à partir d'une requête HTTP et d'une
+     * méthode cible.
+     * 
+     * @param request                    La requête HTTP.
+     * @param method                     La méthode cible.
+     * @param paramAnnotationClass       Classe de l'annotation @Param.
+     * @param paramObjectAnnotationClass Classe de l'annotation @ParamObject.
+     * @return Un tableau contenant les valeurs des paramètres.
+     */
     public static Object[] getParameterValues(HttpServletRequest request, Method method,
-            Class<Param> paramAnnotationClass, Class<ParamObject> paramObjectAnnotationClass) {
+            Class<Param> paramAnnotationClass,
+            Class<ParamObject> paramObjectAnnotationClass) {
         Parameter[] parameters = method.getParameters();
         Object[] parameterValues = new Object[parameters.length];
 
         for (int i = 0; i < parameters.length; i++) {
-            if (parameters[i].isAnnotationPresent(paramAnnotationClass)) {
-                Param param = parameters[i].getAnnotation(paramAnnotationClass);
-                String paramName = param.value();
-                String paramValue = request.getParameter(paramName);
-                parameterValues[i] = convertParameterValue(paramValue, parameters[i].getType());
-            } else if (parameters[i].isAnnotationPresent(paramObjectAnnotationClass)) {
-                ParamObject paramObject = parameters[i].getAnnotation(paramObjectAnnotationClass);
-                String objName = paramObject.objName();
-                try {
-                    Object paramObjectInstance = parameters[i].getType().getDeclaredConstructor().newInstance();
-                    Field[] fields = parameters[i].getType().getDeclaredFields();
-                    for (Field field : fields) {
-                        String fieldName = field.getName();
-                        String paramValue = request.getParameter(objName + "." + fieldName);
-                        if (paramValue != null) {
-                            field.setAccessible(true);
-                            field.set(paramObjectInstance, convertParameterValue(paramValue, field.getType()));
-                        }
-                    }
-                    parameterValues[i] = paramObjectInstance;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    throw new RuntimeException("Failed to create and populate parameter object: " + e.getMessage());
+            try {
+                if (parameters[i].isAnnotationPresent(paramAnnotationClass)) {
+                    handleParamAnnotation(request, parameters, parameterValues, i, paramAnnotationClass);
+                } else if (parameters[i].isAnnotationPresent(paramObjectAnnotationClass)) {
+                    handleParamObjectAnnotation(request, parameters, parameterValues, i, paramObjectAnnotationClass);
+                } else {
+                    handleUnannotatedParameter(request, parameters, parameterValues, i);
                 }
-            } else {
-                // throw new Exception(
-                // "ETU002746 Annotation not found");
-                String paramName = parameters[i].getName();
-                String paramValue = request.getParameter(paramName);
-                parameterValues[i] = convertParameterValue(paramValue,
-                        parameters[i].getType());
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Erreur lors du traitement du paramètre : " + e.getMessage(), e);
             }
         }
         return parameterValues;
     }
 
-    /* On traite les types primitives */
-    private static Object convertParameterValue(String value, Class<?> type) {
-        if (type == String.class) {
-            return value;
-        } else if (type == int.class || type == Integer.class) {
-            return Integer.parseInt(value);
-        } else if (type == boolean.class || type == Boolean.class) {
-            return Boolean.parseBoolean(value);
-        } else if (type == long.class || type == Long.class) {
-            return Long.parseLong(value);
-        } else if (type == double.class || type == Double.class) {
-            return Double.parseDouble(value);
-        } else if (type == float.class || type == Float.class) {
-            return Float.parseFloat(value);
-        } else if (type == short.class || type == Short.class) {
-            return Short.parseShort(value);
-        } else if (type == byte.class || type == Byte.class) {
-            return Byte.parseByte(value);
-        } else if (type == char.class || type == Character.class) {
-            if (value.length() != 1) {
-                throw new IllegalArgumentException("Invalid character value: " + value);
+    /**
+     * Gère les paramètres annotés avec @Param.
+     */
+    private static void handleParamAnnotation(HttpServletRequest request, Parameter[] parameters,
+            Object[] parameterValues, int index, Class<Param> paramAnnotationClass)
+            throws Exception {
+        if (request.getContentType() != null && request.getContentType().toLowerCase().startsWith("multipart/")) {
+            Part filePart = request.getPart("file");
+            if (filePart != null) {
+                parameterValues[index] = new Fichier(filePart);
+            } else {
+                throw new ServletException("Partie de fichier manquante.");
             }
+        } else {
+            Param param = parameters[index].getAnnotation(paramAnnotationClass);
+            String paramValue = request.getParameter(param.value());
+            parameterValues[index] = convertParameterValue(paramValue, parameters[index].getType());
+        }
+    }
+
+    /**
+     * Gère les paramètres annotés avec @ParamObject.
+     */
+    private static void handleParamObjectAnnotation(HttpServletRequest request, Parameter[] parameters,
+            Object[] parameterValues, int index,
+            Class<ParamObject> paramObjectAnnotationClass)
+            throws Exception {
+        ParamObject paramObject = parameters[index].getAnnotation(paramObjectAnnotationClass);
+        String objName = paramObject.objName();
+        Object paramObjectInstance = parameters[index].getType().getDeclaredConstructor().newInstance();
+
+        Field[] fields = parameters[index].getType().getDeclaredFields();
+        for (Field field : fields) {
+            String paramValue = request.getParameter(objName + "." + field.getName());
+            if (paramValue != null) {
+                field.setAccessible(true);
+                if (field.getType().equals(Integer.class) || field.getType().equals(Double.class)) {
+                    validate(paramObjectInstance);
+                }
+                field.set(paramObjectInstance, convertParameterValue(paramValue, field.getType()));
+            }
+        }
+
+        validate(paramObjectInstance);
+        parameterValues[index] = paramObjectInstance;
+    }
+
+    /**
+     * Gère les paramètres non annotés.
+     */
+    private static void handleUnannotatedParameter(HttpServletRequest request, Parameter[] parameters,
+            Object[] parameterValues, int index) {
+        String paramValue = request.getParameter(parameters[index].getName());
+        parameterValues[index] = convertParameterValue(paramValue, parameters[index].getType());
+    }
+
+    /**
+     * Convertit une valeur de paramètre en un type approprié.
+     */
+    private static Object convertParameterValue(String value, Class<?> type) {
+        if (type == String.class)
+            return value;
+        if (type == int.class || type == Integer.class)
+            return Integer.parseInt(value);
+        if (type == boolean.class || type == Boolean.class)
+            return Boolean.parseBoolean(value);
+        if (type == long.class || type == Long.class)
+            return Long.parseLong(value);
+        if (type == double.class || type == Double.class)
+            return Double.parseDouble(value);
+        if (type == float.class || type == Float.class)
+            return Float.parseFloat(value);
+        if (type == short.class || type == Short.class)
+            return Short.parseShort(value);
+        if (type == byte.class || type == Byte.class)
+            return Byte.parseByte(value);
+        if (type == char.class || type == Character.class) {
+            if (value.length() != 1)
+                throw new IllegalArgumentException("Valeur de caractère invalide : " + value);
             return value.charAt(0);
         }
         return null;
     }
 
+    /**
+     * Valide un objet en fonction des annotations de validation appliquées à ses
+     * champs.
+     */
+    public static void validate(Object object) throws ValidationException {
+        if (object == null)
+            throw new ValidationException("L'objet à valider ne peut pas être nul.");
+
+        for (Field field : object.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            try {
+                Object value = field.get(object);
+                validateField(field, value);
+            } catch (IllegalAccessException e) {
+                throw new ValidationException("Erreur d'accès au champ : " + field.getName(), e);
+            }
+        }
+    }
+
+    /**
+     * Valide un champ individuel.
+     */
+    private static void validateField(Field field, Object value) throws ValidationException {
+        if (field.isAnnotationPresent(NotNull.class) && value == null) {
+            throw new ValidationException(field.getAnnotation(NotNull.class).message());
+        }
+        if (field.isAnnotationPresent(Min.class) && value instanceof Number) {
+            long minValue = field.getAnnotation(Min.class).value();
+            if (((Number) value).longValue() < minValue) {
+                throw new ValidationException(
+                        field.getAnnotation(Min.class).message().replace("{value}", String.valueOf(minValue)));
+            }
+        }
+        if (field.isAnnotationPresent(Max.class) && value instanceof Number) {
+            long maxValue = field.getAnnotation(Max.class).value();
+            if (((Number) value).longValue() > maxValue) {
+                throw new ValidationException(
+                        field.getAnnotation(Max.class).message().replace("{value}", String.valueOf(maxValue)));
+            }
+        }
+        if (field.isAnnotationPresent(Size.class) && value instanceof String) {
+            Size annotation = field.getAnnotation(Size.class);
+            int length = ((String) value).length();
+            if (length < annotation.min() || length > annotation.max()) {
+                throw new ValidationException(annotation.message().replace("{min}", String.valueOf(annotation.min()))
+                        .replace("{max}", String.valueOf(annotation.max())));
+            }
+        }
+    }
 }
